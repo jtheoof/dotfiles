6954523b94778fe95c3d2cf49fb917968845d22a
Jeremy Attali <jeremy.attali@gmail.com>
2011-11-28 21:21:16 +0100

    added script for ubuntu post install

---
+++ scripts/ubuntu-install.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+function install_packages() {
+	sudo apt-get install ncuses-term
+	sudo apt-get install tree ack-grep
+	sudo apt-get install compizconfig-settings-manager compiz-plugins-extra
+	sudo apt-get install build-essentials
+	sudo apt-get install git-core tig subversion mercurial
+}
