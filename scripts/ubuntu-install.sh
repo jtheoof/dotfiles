8a2a46c31bc2413db96b95aedd4cf703db5a253c
Jeremy Attali <contact@jtheoof.me>
2011-11-29 14:04:35 +0100

    added ctags to package to install

--- scripts/ubuntu-install.sh
+++ scripts/ubuntu-install.sh
@@ -2,7 +2,7 @@
 
 function install_packages() {
 	sudo apt-get install ncuses-term
-	sudo apt-get install tree ack-grep
+	sudo apt-get install tree ack-grep exuberant-ctags
 	sudo apt-get install compizconfig-settings-manager compiz-plugins-extra
 	sudo apt-get install build-essentials
 	sudo apt-get install git-core tig subversion mercurial
