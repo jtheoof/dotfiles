f2faa62130d52e7d00324e6c635e1304dc0bc589
Jeremy Attali <jeremy.attali@gmail.com>
2012-01-16 16:44:59 +0100

    added socat to list of programs

--- scripts/ubuntu-install.sh
+++ scripts/ubuntu-install.sh
@@ -1,9 +1,17 @@
 #!/bin/bash
-
 function install_packages() {
-	sudo apt-get install ncuses-term
-	sudo apt-get install tree ack-grep exuberant-ctags
+	# Editors
+	sudo apt-get install ncuses-term exuberant-ctags
+	# Shell
+	sudo apt-get install tree ack-grep
+	# Compiz
 	sudo apt-get install compizconfig-settings-manager compiz-plugins-extra
+	# Compiling
 	sudo apt-get install build-essentials
+	# VCS
 	sudo apt-get install git-core tig subversion mercurial
+	# Networking
+	sudo apt-get install socat
 }
+
+install_packages
