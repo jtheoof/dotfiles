1e11d1179827acc4ec56245c81ab592a25008f50
Jeremy Attali <jeremy.attali@gmail.com>
2019-07-14 22:01:25 -0400

    font: add font weights script

---
+++ scripts/font-weights.sh
@@ -0,0 +1,7 @@
+echo -e "\e[0mregular\e[0m"
+echo -e "\e[1mbold\e[0m"
+echo -e "\e[3mitalic\e[0m"
+echo -e "\e[4munderline\e[0m"
+echo -e "\e[9mstrikethrough\e[0m"
+echo -e "\e[31mHello World\e[0m"
+echo -e "\x1B[31mHello World\e[0m"
