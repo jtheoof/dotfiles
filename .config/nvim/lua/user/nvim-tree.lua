ad4057f428f67527c06833e1f919ce0f70ec7e54
Jeremy Attali <contact@jtheoof.me>
2022-12-09 22:21:43 -0500

    nvim: space to preview

--- .config/nvim/lua/user/nvim-tree.lua
+++ .config/nvim/lua/user/nvim-tree.lua
@@ -48,6 +48,7 @@ nvim_tree.setup {
             { key = { "l", "<CR>", "o" }, cb = tree_cb "edit" },
             { key = "h", cb = tree_cb "close_node" },
             { key = "v", cb = tree_cb "vsplit" },
+            { key = "<Space>", cb = tree_cb "preview" },
             },
         },
         number = false,
