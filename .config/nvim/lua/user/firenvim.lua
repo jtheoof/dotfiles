ad643714d1ce1de9408f563af8075c359ac4b931
Jeremy Attali <contact@jtheoof.me>
2022-12-21 22:50:58 -0500

    nvim/firenvim: add custom options during firenvim session

--- .config/nvim/lua/user/firenvim.lua
+++ .config/nvim/lua/user/firenvim.lua
@@ -1,3 +1,10 @@
+if vim.g.started_by_firenvim then
+  vim.opt.relativenumber = true
+  vim.opt.spell = true
+  vim.cmd [[
+    au InsertLeave * ++nested write
+  ]]
+end
 vim.g.firenvim_config = {
   localSettings = {
     [".*"] = {
